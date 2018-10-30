53e71140caf0e47d191d487bc3c6bf44fcffabc5
Amy White <34233806+s518350@users.noreply.github.com>
2018-10-29 19:36:13 -0500

    changes

--- public/js/clock.js
+++ public/js/clock.js
@@ -2,26 +2,34 @@
 var countDate = new Date("Dec 14, 2018 15:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(function () {
   // Get today's date and time
   var currentDate = new Date().getTime();
 
-  // Find the distance between currentDate and the countDate
-  var distance = countDate - currentDate;
+  var clock = distance(countDate, currentDate, x);
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "I have graduated with a Bachelor's Degree in Digital Media Computer Science.";
+  //Function that takes in countDate -- calculate distance
+  function distance(countDate, currentDate, x) {
+    var distance = countDate - currentDate;
+    if (distance < 0) {
+      clearInterval(x);
+      return document.getElementById("countdown").innerHTML = "I have graduated with a Bachelor's Degree in Digital Media Computer Science.";
+    }
+    else {
+      var countDownTime = getTimes(distance);
+      return document.getElementById("countdown").innerHTML = countDownTime[0] + "d " + countDownTime[1] + "h "
+        + countDownTime[2] + "m " + countDownTime[3] + "s ";
+    }
   }
-}, 1000);
+
+  function getTimes(distance) {
+    var timesArray = [];
+    timesArray[0] = Math.floor(distance / (1000 * 60 * 60 * 24));
+    timesArray[1] = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    timesArray[2] = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    timesArray[3] = Math.floor((distance % (1000 * 60)) / 1000);
+    return timesArray;
+  }
+});
+
+
---
+++ test/test.js
@@ -0,0 +1,5 @@
+//This test should always pass
+QUnit.test("Here's a test that should always pass", function (assert) {
+    assert.ok(1 == "1", "1=1 success!");
+});
+
